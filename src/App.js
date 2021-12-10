3ed952f9931c2218942907008ab2d050937a944f
Minh <minhsiro99@gmail.com>
2021-12-11 01:26:47 +0700

    loading 95%

--- src/App.js
+++ src/App.js
@@ -2,53 +2,66 @@ import './App.css';
 import * as React from 'react';
 import { ReactComponent as Twitter } from './twitter.svg';
 import { ReactComponent as Tumblr } from './tumblr.svg';
-import { useState } from 'react';
-// import {ReactDOM} from 'react-dom';
+import Quotes from './Quotes'
 
-const randomQuote =
-  [{text: ' When I let go of what I am, I become what I might be.',
-  author:"- minhsiro",
-  color:"green",
-  },
-  {text: ' When I let go of what I am, I become what I might be.',
-  author:"- minhsiro",
-  color:"pink",
-  },
-  {text: ' When I let go of what I am, I become what I might be.',
-  author:"- minhsiro",
-  color:"yellow",
-  }];
+
+let firstQuote = Math.floor(Math.random()*Quotes.length);
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "NEW_QUOTE":
+      let nextQuote = Math.floor(Math.random()*Quotes.length);
+      while (state.id === Quotes[nextQuote].id){
+        nextQuote = Math.floor(Math.random()*Quotes.length);
+      }
+      const newState = Object.assign({},Quotes[nextQuote]);
+      return newState;
+    default:
+      return state;
+  }
+}
 
 
 function App() {
-  const [state,setState] = useState(
-    {text: 'When I let go of what I am, I become what I might be.',
-    author:"- minhsiro",
-    color:"violet",
-    }
-    );
-    let tweetIntent = "https://twitter.com/intent/tweet?text="
-    const tweetButton = tweetIntent.concat(state.text.replace(/\s+/g,'%20'));
+    const [state, dispatch] = React.useReducer(reducer, Quotes[firstQuote]);
+    const [active,setActive] = React.useState(true);
+
+    // twitter button set up
+    const tweetButton = "https://twitter.com/intent/tweet?text=".concat(state.text.replace(/\s+/g,'%20'));
+
+    // tumblr button set up
+    const tumblrButton = `https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${state.author.replace(/\s+/g,'%20')}&content=${state.text.replace(/\s+/g,'%20')}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`;
+
+    React.useEffect(() => {
+      setActive(() => true);
+    },[state.id]);
+
     const handleNewQuote = () => {
-      let nextQuote = Math.round(Math.random() * (randomQuote.length-1));
-      setState(() => {
-        return randomQuote[nextQuote];
-      })
+      setActive(() => false);
+      setTimeout(() => dispatch({type: 'NEW_QUOTE'}),800);
     };
+
   return (
-    <div className="App" style={{backgroundColor:state.color}}>
-      <div className='quote-box'>
-        <p className="quote" style={{color:state.color}}>&#8220;{state.text}</p>
-        <p className="author" style={{color:state.color}}>{state.author}</p>
-      {/* <a className="twitter-link">twitter</a>
-      <a className= "tumblr-link">tumblr</a> */}
-      <a href={tweetButton} target="_blank" title="Tweet this quote!"><Twitter className="social-media" style={{backgroundColor:state.color}}/></a>
-      <a href="#123" title="Post this quote on tumblr!"><Tumblr className="social-media" style={{backgroundColor:state.color}}/></a>
-      <button className="new-quote-btn" onClick={handleNewQuote} style={{backgroundColor:state.color}}>New quote</button>
-      </div>
-      <p style={{color:'white'}}>by Minh</p>
-    </div>
-  );
-}
+        <div className={`App`} style={{backgroundColor:state.color}} key={Math.random()}>
+          <div className={`quote-box ${active ? 'fade-in' : 'fade-out'}`}>
+            <p className="quote" style={{color:state.color}}>&#8220;{state.text}</p>
+            <p className="author" style={{color:state.color}}><i>{state.author}</i></p>
+
+            <a href={tweetButton} target="_blank" title="Tweet this quote!">
+            <Twitter className="social-media" style={{backgroundColor:state.color}}/>
+            </a>
+
+            <a className="tumblr-share-button" href={tumblrButton} target="_blank" title="Post this quote on tumblr!">
+            <Tumblr className="social-media" style={{backgroundColor:state.color}}/>
+            </a>
+
+            <button className="new-quote-btn" onClick={handleNewQuote} style={{backgroundColor:state.color, border:state.color}}>New quote</button>
+          </div>
+          <p style={{color:'white'}}>by Minh</p>
+          {console.log("in")}
+        </div>
+    )
+  }
+
 
 export default App;
+
---
+++ src/Quotes.js
@@ -0,0 +1,54 @@
+const Quotes =
+  [{text: 'Opportunities don’t happen, you create them.',
+  author:"- Chris Grosser",
+  color:"#28B463",
+  id: 0
+  },
+  {text: "It doesn't matter how slowly you go, as long as you don't stop",
+  author:"- Anonymous",
+  color:"#F08080",
+  id: 1
+  },
+  {text: 'EVERYTHING IS HARD BEFORE IT IS EASY.',
+  author:"- Anonymous",
+  color:"#DE3163",
+  id: 2
+  },
+  {text: 'Don\'t pray for an easy life, pray for the strength to endure a difficult one.',
+  author:"- Bruce Lee",
+  color:"#A808B5",
+  id: 3
+  },
+  {text: 'Whether you think you can or you can\'t: you are always right.',
+  author:"- Anonymous",
+  color:"#6B5B95",
+  id: 4
+  },
+  {text: 'Just because most don\'t make it, doesn\'t mean you can\'t.',
+  author:"- Anonymous",
+  color:"#F7CAC9",
+  id: 5
+  },
+  {text: 'Tough people aren\'t built in easy situations.',
+  author:"- Anonymous",
+  color:"#5B5EA6",
+  id: 6
+  },
+  {text: 'Do not give up, the beginning is always the hardest.',
+  author:"- Anonymous",
+  color:"#B565A7",
+  id: 7
+  },
+  {text: 'Work like you don\'t need the money, love like you\'ve never been hurt and dance like nobody\'s watching.',
+  author:"- Anonymous",
+  color:"#9B2335",
+  id: 8
+  },
+  {text: 'If you obey all the rules, you will miss all the fun.',
+  author:"- Anonymous",
+  color:"#88B04B",
+  id: 9
+  }
+];
+
+  export default Quotes;
